ce10db0256ce282b2e80fd834bcf8ae59d56a974
James-Michael-3301 <jamariego02@gmail.com>
2026-01-15 16:02:27 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Bank of James III. All rights reserved to Almariego James of WD201.</p>
+</footer>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Bank of James III</title>
+    <link href="css/styles.css?a=1" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <a href="bank.php"><img src="img/logo.png" alt="Bank of James III" /></a>
+    </header>
+    <section>
